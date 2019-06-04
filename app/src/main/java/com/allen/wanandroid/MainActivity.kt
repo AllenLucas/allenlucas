ea50cb8ee586f10094762019f7401036630f05b9
liuanlin <l476849560@126.com>
2019-06-04 17:29:40 +0800

    第一次上传

---
+++ app/src/main/java/com/allen/wanandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.allen.wanandroid
+
+import com.allen.wanandroid.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun getLayout() = R.layout.activity_main
+
+    override fun initView() {
+    }
+
+    override fun initListener() {
+    }
+}
---
+++ app/src/main/java/com/allen/wanandroid/base/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.allen.wanandroid.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    @LayoutRes
+    abstract fun getLayout(): Int
+    abstract fun initView()
+    abstract fun initListener()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayout())
+        initView()
+        initListener()
+    }
+}
